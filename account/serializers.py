9c63f0d5f34a00ceaee49d728c2e6cf166c20084
Sourcery AI <>
2022-09-17 06:33:58 +0000

    'Refactored by Sourcery'

--- account/serializers.py
+++ account/serializers.py
@@ -66,10 +66,10 @@ class SendPasswordResetEmailSerializer(serializers.Serializer):
       print('Encoded UID', uid)
       token = PasswordResetTokenGenerator().make_token(user)
       print('Password Reset Token', token)
-      link = 'http://localhost:3000/api/user/reset/'+uid+'/'+token
+      link = f'http://localhost:3000/api/user/reset/{uid}/{token}'
       print('Password Reset Link', link)
       # Send EMail
-      body = 'Click Following Link to Reset Your Password '+link
+      body = f'Click Following Link to Reset Your Password {link}'
       data = {
         'subject':'Reset Your Password',
         'body':body,
--- account/views.py
+++ account/views.py
@@ -32,11 +32,10 @@ class UserLoginView(APIView):
     email = serializer.data.get('email')
     password = serializer.data.get('password')
     user = authenticate(email=email, password=password)
-    if user is not None:
-      token = get_tokens_for_user(user)
-      return Response({'token':token, 'msg':'Login Success'}, status=status.HTTP_200_OK)
-    else:
+    if user is None:
       return Response({'errors':{'non_field_errors':['Email or Password is not Valid']}}, status=status.HTTP_404_NOT_FOUND)
+    token = get_tokens_for_user(user)
+    return Response({'token':token, 'msg':'Login Success'}, status=status.HTTP_200_OK)
 
 class UserProfileView(APIView):
   renderer_classes = [UserRenderer]
